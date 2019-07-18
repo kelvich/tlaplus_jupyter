e6371fde7d6fe165078ea0dc8659bac2a60a1115
Stas Kelvich <stanconn@gmail.com>
2019-07-18 21:25:14 +0300

    fix deps

--- setup.py
+++ setup.py
@@ -12,10 +12,11 @@ setup(
     author_email='stas.kelvich@gmail.com',
     url='https://github.com/kelvich/tlaplus_kernel',
     long_description=readme,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     test_suite='tests',
     install_requires=[
-        'jupyter_client', 'IPython', 'ipykernel'
+        'jupyter_client', 'IPython', 'ipykernel', 'future'
     ],
     zip_safe=False
 )
