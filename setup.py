f3a0be8e98ca9c26e060c2bb911068d60c63b787
Stas Kelvich <stanconn@gmail.com>
2019-11-11 10:41:24 +0300

    list acceptable python versions in setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,11 @@ setup(
     author='Stas Kelvich',
     author_email='stas.kelvich@gmail.com',
     url='https://github.com/kelvich/tlaplus_jupyter',
+    keywords=['jupyter', 'tla', 'tlaplus', 'pluscal'],
     long_description=readme,
     long_description_content_type="text/markdown",
     include_package_data=True,
+    python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     test_suite='tests',
     install_requires=[
         # notebook is not actually needed, but it's simpler to install
@@ -25,5 +27,12 @@ setup(
         'future>=0.18',
         'psutil>=5.6.5'
     ],
-    zip_safe=False
+    zip_safe=False,
+    license='BSD',
+    platforms='Platform Independent',
+    classifiers=[
+        'Topic :: Software Development :: Interpreters',
+        'Topic :: Software Development :: Quality Assurance',
+        'Scientific/Engineering :: Mathematics'
+    ]
 )
